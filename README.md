d1715d19938cf984ea9f8dd68875d6df49a2cf8b
royal-lobster <srujangs8@gmail.com>
2022-03-07 22:48:14 +0530

    added PAY Hill Cipher

---
+++ README.md
@@ -0,0 +1,29 @@
+# Cryptography and Network Security Lab Experiments
+
+This repository consist of all programs that I implemented for questions in lab syllabus.
+
+### Syllabus
+
+- [ ] "Hello world" XOR each char with 0
+- [ ] "Hello world" XOR, AND, OR each char with 127
+- [ ] Hill Cipher with given key and message
+- [ ] Hill Cipher
+- [ ] Ceaser Cipher
+- [ ] Substitution Cipher
+- [ ] Affine Cipher with given equation
+- [ ] Playfair Cipher with given key
+- [ ] Polyalphabetic Cipher
+- [ ] Autokey Cipher
+- [ ] Rail fence technique
+- [ ] Simple Columner Transposition technique
+- [ ] Advanced Columner Transposition technique
+- [ ] DES algorithm
+- [ ] Blowfish algorithm
+- [ ] Rijndael algorithm
+- [ ] RC4 logic in JAVA
+- [ ] Euclidean Algorithm
+- [ ] Advanced Euclidean Algorithm
+- [ ] Simple RSA Algorithm with small numbers
+- [ ] RSA Algorithm, Diffie-Hellman Key Exchange Mechanism using HTML and JS
+- [ ] Caluculate Message Digest of text using SHA-I in JAVA
+- [ ] Caluculate the Message Digest of text using MD5 in JAVA
---
+++ src/PAY_HillCipher.java
@@ -0,0 +1,96 @@
+
+public class PAY_HillCipher {
+    public static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    double[][] matrixMultiply(double[][] a, double[][] b) {
+        double[][] c = new double[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return c;
+    }
+
+    String matrixToString(double[][] a) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                sb.append(alphabet.charAt((int) a[i][j] % 26));
+            }
+        }
+        return sb.toString();
+    }
+
+    double[][] inverseMatrix(double[][] a) {
+        double det = 0;
+        double inv[][] = new double[3][3];
+
+        // Find determinant of matrix a[][]
+        for (int i = 0; i < 3; i++)
+            det = det + (a[0][i]
+                    * (a[1][(i + 1) % 3] * a[2][(i + 2) % 3] - a[1][(i + 2) % 3] * a[2][(i + 1) % 3]));
+
+        // Find inverse using adjoint method
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j)
+                inv[i][j] = ((a[(j + 1) % 3][(i + 1) % 3] * a[(j + 2) % 3][(i + 2) % 3])
+                        - (a[(j + 1) % 3][(i + 2) % 3] * a[(j + 2) % 3][(i + 1) % 3])) / det;
+        }
+        return inv;
+    }
+
+    // main function of program
+    public static void main(String[] args) {
+        // create a new PAY_HillCipher object
+        PAY_HillCipher obj = new PAY_HillCipher();
+
+        // given key in the question (3x3 matrix)
+        double[][] keyMatrix = { { 17, 17, 5 }, { 21, 18, 21 }, { 2, 2, 19 } };
+
+        // given plaintext in the question
+        String plainText = "PAY";
+        System.out.println("Plaintext: " + plainText);
+
+        // ------------------
+        // ENCRYPTION PHASE
+        // ------------------
+
+        // convert plaintext to (3x1) matrix for MM
+        double[][] plainTextMatrix = { { 15 }, { 0 }, { 24 } };
+
+        // perform MM on keyMatrix and plainTextMatrix
+        double[][] cipherTextMatrix = obj.matrixMultiply(keyMatrix, plainTextMatrix);
+
+        // convert cipherTextMatrix to plain cipher text
+        String cipherText = obj.matrixToString(cipherTextMatrix);
+        System.out.println("Cipher Text: " + cipherText);
+
+        // ------------------
+        // DECRYPTION PHASE
+        // ------------------
+
+        // inverse the keyMatrix for decryption
+        double[][] inverseKeyMatrix = obj.inverseMatrix(keyMatrix);
+
+        // perform matrix multiplication on inverseKeyMatrix and cipherTextMatrix
+        double[][] decryptedTextMatrix = obj.matrixMultiply(inverseKeyMatrix, cipherTextMatrix);
+
+        // convert decryptedTextMatrix to plain text
+        String decryptedText = obj.matrixToString(decryptedTextMatrix);
+        System.out.println("Decrypted Text: " + decryptedText);
+
+        // ------------------
+        // TESTING PHASE
+        // ------------------
+
+        if (decryptedText.equals(plainText)) {
+            System.out.println("Test passed!");
+        } else {
+            System.out.println("Test failed!");
+        }
+
+    }
+}
