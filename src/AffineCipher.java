49a1e277333aa682dd1703df76ccfd5096d77d24
Urban-lobster <102146363+Urban-lobster@users.noreply.github.com>
2022-03-22 16:03:00 +0530

    formated and added license

--- src/AffineCipher.java
+++ src/AffineCipher.java
@@ -1,75 +1,77 @@
 import java.util.Scanner;
 
+/**
+ * Affine Cipher algorithm that encrypts plaintext by given key
+ * and also decrypts cipher text.
+ *
+ * @author Srujan Gurram
+ * @version 1.0
+ * @license MIT
+ */
+
 public class AffineCipher {
 
     public static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- 
-    public int findPos(char a){
-        for(int i = 0; i<alphabet.length(); i++)
-            if(alphabet.charAt(i) == a)
+
+    public int findPos(char a) {
+        for (int i = 0; i < alphabet.length(); i++)
+            if (alphabet.charAt(i) == a)
                 return i;
         return -1;
     }
 
-    public String encrypt(String msg, int a, int b)
-    {
+    public String encrypt(String msg, int a, int b) {
         msg = msg.toUpperCase();
         StringBuilder output = new StringBuilder();
-        for(int i = 0; i < msg.length(); i++){
-            if(msg.charAt(i) != ' '){
+        for (int i = 0; i < msg.length(); i++) {
+            if (msg.charAt(i) != ' ') {
                 int pos = findPos(msg.charAt(i));
                 int cipherPos = (a * pos + b) % 26;
                 char cipherChar = alphabet.charAt(cipherPos);
                 output.append(cipherChar);
-            }
-            else
+            } else
                 output.append(" ");
         }
         return output.toString();
     }
- 
-    public String decrypt(String cipher, int a, int b)
-    {
-        //---------------------------------
+
+    public String decrypt(String cipher, int a, int b) {
+        // ---------------------------------
         // Find mulitplicative inverse of a
-        //---------------------------------
+        // ---------------------------------
         int a_inv = 0;
         int flag = 0;
-        for (int i = 0; i < 26; i++)
-        {
+        for (int i = 0; i < 26; i++) {
             flag = (a * i) % 26;
- 
+
             // Check if (a*i)%26 == 1,
             // then i will be the multiplicative inverse of a
-            if (flag == 1)
-            {
+            if (flag == 1) {
                 a_inv = i;
             }
         }
 
-        //---------------------------------
+        // ---------------------------------
         // Decrypt the text with a_inv and b
-        //---------------------------------
+        // ---------------------------------
         StringBuilder output = new StringBuilder();
-        for(int i = 0; i < cipher.length(); i++){
-            if(cipher.charAt(i) != ' '){
+        for (int i = 0; i < cipher.length(); i++) {
+            if (cipher.charAt(i) != ' ') {
                 int pos = findPos(cipher.charAt(i));
                 int cipherPos = (a_inv * (pos - b)) % 26;
-                 // if cipherPos is negative, add 26 to it
-                 if(cipherPos < 0)
+                // if cipherPos is negative, add 26 to it
+                if (cipherPos < 0)
                     cipherPos += 26;
                 char cipherChar = alphabet.charAt(cipherPos);
                 output.append(cipherChar);
-            }
-            else
+            } else
                 output.append(" ");
         }
         return output.toString();
     }
- 
+
     // Driver code
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Initalize this object
         AffineCipher obj = new AffineCipher();
 
@@ -119,4 +121,3 @@ public class AffineCipher {
         input.close();
     }
 }
- 
