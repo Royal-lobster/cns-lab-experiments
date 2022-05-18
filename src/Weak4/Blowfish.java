dd71b01d04400e0a13482f16dff8f84bdd18f127
Srujan Gurram <srujangs8@gmail.com>
2022-05-18 21:33:29 +0530

    added blowfish

---
+++ src/Weak4/Blowfish.java
@@ -0,0 +1,61 @@
+package Weak4;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class Blowfish {
+    private Cipher blowfish;
+    private SecretKey key;
+    private final int KEY_SIZE = 56;
+
+    Blowfish() throws Exception {
+        blowfish = Cipher.getInstance("Blowfish");
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
+        keyGenerator.init(KEY_SIZE);
+        key = keyGenerator.generateKey();
+    }
+
+    public String encrypt(String p) throws Exception {
+
+        // Convert String to byte array
+        byte[] pInBytes = p.getBytes();
+
+        // Initialize encryption cipher
+        blowfish.init(Cipher.ENCRYPT_MODE, key);
+
+        // Encrypt data
+        byte[] encryptedBytes = blowfish.doFinal(pInBytes);
+
+        // Convert encrypted bytes to base64 encoded string
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public String decrypt(String c) throws Exception {
+
+        // Decode base64 encoded cipher text
+        byte[] cInBytes = Base64.getDecoder().decode(c);
+
+        // Initialize decryption cipher (with Initialization Vector)
+        blowfish.init(Cipher.DECRYPT_MODE, key);
+
+        // Decrypt data
+        byte[] decryptedBytes = blowfish.doFinal(cInBytes);
+
+        // Convert decrypted bytes to string
+        return new String(decryptedBytes);
+    }
+
+    public static void main(String[] args) {
+        try {
+            DES obj = new DES();
+            String c = obj.encrypt("Hello This is Secret Message");
+            String d = obj.decrypt(c);
+
+            System.out.println("Encrypted Data : " + c);
+            System.out.println("Decrypted Data : " + d);
+        } catch (Exception ignored) {
+        }
+    }
+}
